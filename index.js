7600ac14a3e4d2708fe2d5737d9383ff03919f28
Abhishekgiri22 <49429716+Abhishekgiri22@users.noreply.github.com>
2020-09-24 19:52:51 +0545

    backend API
    Backend code for API

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require('morgan');
+const userRouter = require('./routes/users');
+const roomRouter = require('./routes/room');
+const roomsRouter = require('./routes/rooms');
+const feedbackRouter = require('./routes/feedback');
+const dotenv = require('dotenv').config();
+const uploadRouter = require('./routes/upload');
+const auth = require('./auth');
+const cors = require('cors');
+
+
+
+const app = express();
+app.use(morgan('tiny'));
+app.use(express.json());
+app.options('*', cors());
+app.use(express.urlencoded({extended: true }));
+
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then((db) => {
+        console.log("Successfully connected to MongodB server");
+    }, (err) => console.log(err));
+
+app.use('/users', userRouter);
+app.use('/room', roomRouter);
+app.use('/rooms', roomsRouter);
+app.use('/upload', uploadRouter);
+app.use('/feedback', feedbackRouter);
+app.use(auth.verifyUser);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.statusCode = 500;
+    res.json({ status: err.message });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`App is running at localhost:${process.env.PORT}`);
+});
